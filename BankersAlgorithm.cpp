b1a0d6c32f8d43b7aa75e3a889db4bce029bc366
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-01-05 02:35:10 +0000

    Added newRequest(), resourceRequestAlgorithm(), minor code changes

--- BankersAlgorithm.cpp
+++ BankersAlgorithm.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int allocation[5][5], maximum[5][5], need[5][5], available[5], initialResources[5];
-int processes, resources, numbbb;
+int allocation[5][5], maximum[5][5], need[5][5], available[5], initialResources[5], requestedResources[5], safeSequence[5];
+int processes, resources, processRequested;
 
 // A function to print the table to the console for the users benefit.
 void table()
 {
 	cout << "\n\n\tBankers Table\n";
 	for (int i = 0; i < resources; ++i)
-    {
+	{
 		// Reset the initial resources [i] for all resources, to allow for correct addition.
-        initialResources[i] = 0;
-    };
-    // Sum up allocations and available resources to get the initial resources.
-    for (int i = 0; i < resources; ++i)
-    {
-        for (int j = 0; j < processes; j++)
-        {
-            initialResources[i] += allocation[j][i];
-        };
-        initialResources[i] += available[i];
-    };
-	// Display the initial resources for the system 
+		initialResources[i] = 0;
+	};
+	// Sum up allocations and available resources to get the initial resources.
+	for (int i = 0; i < resources; ++i)
+	{
+		for (int j = 0; j < processes; j++)
+		{
+			initialResources[i] += allocation[j][i];
+		};
+		initialResources[i] += available[i];
+	};
+	// Display the initial resources for the system
 	cout << "Initial Resources: ";
-    for (int j = 0; j < resources; j++)
-    {
-        cout << initialResources[j] << " ";
-    };
+	for (int j = 0; j < resources; j++)
+	{
+		cout << initialResources[j] << " ";
+	};
 	cout << "\n\n";
 	// Display the titles for each column of the table.
 	cout << "Process   Allocation   maximum   Need\n";
@@ -50,10 +50,10 @@ void table()
 			cout << need[i][j] << " ";
 		};
 
-		cout << "\n\n";
+		cout << "\n";
 	};
 	// Display the available resources to the user beneath the table.
-	cout << "Available Resources: ";
+	cout << "\nAvailable Resources: ";
 	for (int i = 0; i < resources; ++i)
 	{
 		cout << available[i] << " ";
@@ -90,81 +90,134 @@ void getInput()
 			need[i][j] = maximum[i][j] - allocation[i][j];
 		};
 	};
-
-	cout << "Enter the amount of available resources the system has: ";
-
+	cout << "\nPlease enter the amount of available resources the system has.\n";
 	for (int i = 0; i < resources; i++)
 	{
+		cout << "Resource " << i << ": ";
 		cin >> available[i];
 	};
 };
-
 
 // A function to attain the safety state of the system.
 bool checkForSafeState()
 {
 	// Declaring work and finish as arrays, the same size as their respective needs [i].
-    int work[resources];
-    bool finish[processes] = {false}; // finish[] will be the same length as processes and each index will be boolean false to start.
+	int work[resources];
+	bool finish[processes] = {false}; // finish[] will be the same length as processes and each index will be boolean false to start.
+	int l = 0;
 
-    // Assigning each index of work to be equal to each index of available.
-    for (int i = 0; i < resources; i++)
-    {
-        work[i] = available[i];
-    }
+	// Assigning each index of work to be equal to each index of available.
+	for (int i = 0; i < resources; i++)
+	{
+		work[i] = available[i];
+	}
 	// A while (true) loop to keep the safety algorithm going until instructed to break.
-    while (true)
-    {
-        bool found = false;
-   	 	// Find an index where finish[i] is equal to false, and need[i] is less than or equal to work[i].
-        for (int i = 0; i < processes; i++)
-        {
-            if (!finish[i])
-            {
-                bool possible = true;
-                for (int j = 0; j < resources; j++)
-                {
-                    if (need[i][j] > work[j])
-                    {
-                        possible = false;
-                        break;
-                    }
-                }
-                if (possible)
-                {
-                    // If all need[i] is less than or equal to work[i], add allocation[i] to work[i] and set finish[i] to true.
-                    for (int k = 0; k < resources; k++)
-                    {
-                        work[k] += allocation[i][k];
-                        finish[i] = true;
-                        found = true;
-                    }
-                }
-            }
-        }
-        // If no processes were possible in this pass (found never updated to true), break the while loop
-        if (!found)
-        {
-            break;
-        }
-    }
+	while (true)
+	{
+		bool found = false;
+		// Find an index where finish[i] is equal to false, and need[i] is less than or equal to work[i].
+		for (int i = 0; i < processes; i++)
+		{
+			if (!finish[i])
+			{
+				bool possible = true;
+				for (int j = 0; j < resources; j++)
+				{
+					if (need[i][j] > work[j])
+					{
+						possible = false;
+						break;
+					}
+				}
+				if (possible)
+				{
+					// If all need[i] is less than or equal to work[i], add allocation[i] to work[i] and set finish[i] to true.
+					for (int k = 0; k < resources; k++)
+					{
+						work[k] += allocation[i][k];
+					}
+					finish[i] = true;
+					found = true;
+					safeSequence[l] = i;
+					l++;
+				}
+			}
+		}
+		// If no processes were possible in this pass (found never updated to true), break the while loop
+		if (!found)
+		{
+			break;
+		}
+	}
 
-    // A for loop to check each indexes boolean value within finish.
-    for (int i = 0; i < processes; i++)
-    {
-        if (!finish[i]) // If an index of finish is false then its need surpassed the available resources and it was never able to execute.
-        {
-            return false; // If a finish[i] is equal to false, return false. The system is not in a Safe state.
-        }
-    }
-    return true; // If finish[i] is equal to true for all indexes, then return true. the system is in a Safe state.
+	// A for loop to check each indexes boolean value within finish.
+	for (int i = 0; i < processes; i++)
+	{
+		if (!finish[i]) // If an index of finish is false then its need surpassed the available resources and it was never able to execute.
+		{
+			return false; // If a finish[i] is equal to false, return false. The system is not in a Safe state.
+		}
+	}
+	return true; // If finish[i] is equal to true for all indexes, then return true. the system is in a Safe state.
+};
+
+// A function to hold the resource request algorithm.
+// This function will determine inf the resource request can be granted and if so will update the needed matrixes.
+void resourceRequestAlgorithm()
+{
+	bool needCheck = true, availableCheck = true;
+	for (int i = 0; i < resources; i++)
+	{
+		if (requestedResources[i] > need[processRequested][i])
+		{
+			needCheck = false;
+		}
+		if (requestedResources[i] > available[i])
+		{
+			availableCheck = false;
+		}
+	}
+
+	if (availableCheck == true && needCheck == true)
+	{
+		for (int i = 0; i < resources; i++)
+		{
+			available[i] -= requestedResources[i];
+			allocation[processRequested][i] += requestedResources[i];
+			need[processRequested][i] -= requestedResources[i];
+		}
+		cout << "\nRequest granted.";
+	}
+	else
+	{
+		cout << "\nRequest cannot be granted.\nPlease ensure your request is less than or equal to the need of that process\nand less than or equal to the available resources.";
+	}
 };
 
 // A function to attain a new request from the user.
 void newRequest()
 {
+	cout << "\nPlease enter the process you wish to make a request on (0 to " << processes - 1 << "): ";
+	cin >> processRequested;
+	if (processRequested < 0 || processRequested >= processes)
+	{
+		cout << "Invalid process number <<(0 to " << processes - 1 << ")>>\n";
+		return;
+	}
 
-	cout << "not Yet Complete\n";
+	cout << "Please enter the new request figures for Process " << processRequested << ".\n";
+	for (int i = 0; i < resources; i++)
+	{
+		cout << "Resource allocation " << i << ": ";
+		cin >> requestedResources[i];
+		if (requestedResources[i] < 0 || requestedResources[i] > need[processRequested][i])
+		{
+			cout << "Invalid request amount. It cannot be more than the need or a negative value.\n";
+			return;
+		}
+	}
+
+	resourceRequestAlgorithm();
 };
 
 // A function to attain and present the outcome of the bankers algorithm for the user.
@@ -173,10 +226,20 @@ void calculateBAOutcome()
 	// If the system is in a safe state, inform the user and allow for a new request.
 	if (checkForSafeState())
 	{
-		cout << "Your request has been granted, the system is in a safe state.\n";
+		cout << "Your request has been granted, the system is in a safe state.\nThe safe sequence found is ";
+		for (int i = 0; i < processes; i++)
+		{
+			cout << "P" << safeSequence[i];
+			if (i < processes - 1)
+			{
+				cout << " --> ";
+			}
+			else {
+				cout << ".";
+			};
+		}
 		newRequest();
 		table();
-		calculateBAOutcome();
 	}
 	// Else no new requests will be granted and the system will exit.
 	else
